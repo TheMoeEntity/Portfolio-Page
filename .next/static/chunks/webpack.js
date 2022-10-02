f6ad842cda33a38b6679386b5d23c57e2e9dce55
Moses Nwigberi <mosesnwigberi@icloud.com>
2022-10-02 22:52:55 +0100

    Changed partners section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a9955af56540c65"; }
+/******/ 		__webpack_require__.h = function() { return "8a10da6cdf0482a4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
