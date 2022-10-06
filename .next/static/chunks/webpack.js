fb34f2280651f77acaa496448e396aa137620b89
Moses Nwigberi <mosesnwigberi@icloud.com>
2022-10-06 11:33:51 +0100

    fixed sidebar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3184e4ce23640aef"; }
+/******/ 		__webpack_require__.h = function() { return "5b6b46f81d218d35"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
