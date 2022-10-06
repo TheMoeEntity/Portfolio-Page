d9568a0f8eb8761fbf0d37b58789660af211ec06
Moses Nwigberi <mosesnwigberi@icloud.com>
2022-10-06 10:54:34 +0100

    Added dark mode auto switch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b93c07ff18bb5037"; }
+/******/ 		__webpack_require__.h = function() { return "fc3e68e455728c96"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
