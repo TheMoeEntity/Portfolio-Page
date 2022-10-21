85af682c36d7cfa327072e9d0ac67ef5e0e394ac
Moses Nwigberi <mosesnwigberi@gmail.com>
2022-10-21 09:07:49 +0100

    changed rgba

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45ab63c2a19e2101"; }
+/******/ 		__webpack_require__.h = function() { return "8a5ba899d84202c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
